2e0701f7f763b8e056f44cd8c230703eb3901918
Hermes <hermesjalvarez@gmail.com>
2015-10-04 21:28:55 -0400

    chapter 19

---
+++ Alcolator/ViewController.h
@@ -0,0 +1,12 @@
+//  ViewController.h
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UITextField *beerPercentTextField;
+@property (weak, nonatomic) IBOutlet UISlider *beerCountSlider;
+@property (weak, nonatomic) IBOutlet UILabel *resultLabel;
+
+@end
+
